fba6dfcc69c1fbbf720cf88dcd7b475da308d4a6
Leonid06 <x.worklene@gmail.com>
2023-06-10 19:20:25 +0300

    added some postgresql env variables

--- backend/external_api_microservice/main.py
+++ backend/external_api_microservice/main.py
@@ -1,11 +1,26 @@
 from typing import Union
-
 from fastapi import FastAPI
+import os
+from pydantic import BaseModel
+import sqlalchemy
+from dotenv import load_dotenv
+import databases
 
-app = FastAPI()
+POSTGRES_USERNAME = os.getenv('POSTGRESQL_USERNAME')
+POSTGRES_PASSWORD = os.getenv('POSTGRESQL_PASSWORD')
+POSTGRES_HOST = os.getenv('POSTGRESQL_HOST')
+POSTGRES_PORT = os.getenv('POSTGRESQL_PORT')
+POSTGRES_DATABASE_NAME = os.getenv('POSTGRESQL_PORT')
+DATABASE_URL = f'postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE_NAME}'
 
-@app.get('/')
-def read_root():
-    return {'Hello' : 'World'}
+def main():
+
+    app = FastAPI()
+
+    @app.get('/')
+    def read_root():
+        return {'Hello' : 'World'}
+
+
 
 
