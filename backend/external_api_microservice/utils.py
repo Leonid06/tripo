aa2f551821c7305d3523c659888032b6e6df5dd5
Leonid06 <x.worklene@gmail.com>
2023-06-16 12:59:38 +0300

    added utility function for jwt

---
+++ backend/external_api_microservice/utils.py
@@ -0,0 +1,45 @@
+from passlib.context import CryptContext
+import os
+from datetime import datetime, timedelta
+from typing import Union, Any
+from jose import jwt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+password_context = CryptContext(schemes=['bcrypt'], deprecated=['auto'])
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
+REFRESH_TOKEN_EXPIRE_MINUTES = int(os.getenv('REFRESH_TOKEN_EXPIRE_MINUTES'))
+ALGORITHM = os.getenv('JWT_ALGORITHM')
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+JWT_REFRESH_SECRET_KEY = os.getenv('JWT_REFRESH_SECRET_KEY')
+
+
+def get_hashed_password(password: str) -> str:
+    return password_context.hash(password)
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return password_context.verify(password, hashed_password)
+
+
+def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    data_to_encode = {'exp': expires_delta, 'sub': str(subject)}
+    encoded_jwt = jwt.encode(data_to_encode, JWT_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    data_to_encode = {'exp': expires_delta, 'sub': str(subject)}
+    encoded_jwt = jwt.encode(data_to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
