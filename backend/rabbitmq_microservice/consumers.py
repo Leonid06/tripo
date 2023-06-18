873574fb7480ad15a67f4f19957a0e84866e0e44
Leonid06 <x.worklene@gmail.com>
2023-06-18 19:46:52 +0300

    added topic-specific consumer and producer classes

--- backend/rabbitmq_microservice/consumers.py
+++ backend/rabbitmq_microservice/consumers.py
@@ -1,22 +1,90 @@
 import pika
+from pika.exceptions import  AMQPError
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
+
 class BaseConsumer:
-    def __init__(self, queue_name):
+    def __init__(self, queue_name=''):
         self.__exchange_type = ''
         self.__host = os.getenv('RABBITMQ_HOST')
-        self.__connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.__host))
-        self.__channel = self.__connection.channel()
         self.__queue_name = queue_name
-        self.__channel.queue_declare(queue=self.__queue_name)
+        self.__connection = None
+        self.__channel = None
 
     def consume(self, callback):
-        self.__channel.basic_consume(
-            queue=self.__queue_name,
-            on_message_callback=callback
-        )
-        self.__channel.start_consuming()
+        try:
+            self.__channel.queue_declare(queue=self.__queue_name)
+            self.__channel.basic_consume(
+                queue=self.__queue_name,
+                on_message_callback=callback
+            )
+            self.__channel.start_consuming()
+        except AMQPError as amqpError :
+            pass
+
+    def open_connection(self):
+        try:
+            self.__connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.__host))
+            self.__channel = self.__connection.channel()
+        except AMQPError as amqpError :
+            pass
+
+    def close_connection(self):
+        try:
+            self.__connection.close()
+        except AMQPError as amqpError :
+            pass
+
+    @property
+    def queue_name(self):
+        return self.__queue_name
+
+    @queue_name.setter
+    def queue_name(self, value):
+        self.__queue_name = value
+
+
+class TopicConsumer(BaseConsumer):
+
+    def __init__(self, topic= None, exchange = None):
+        super().__init__()
+        self.__topic = topic
+        self.__exchange = exchange
+
+    def consume(self, callback):
+        try:
+            self.__channel.exchange_declare(exchange = self.__exchange, exchange_type= 'topic')
+            result = self.__channel.queue_declare(queue= '', exclusive= True)
+            queue_name = result.method.queue
+
+            self.__channel.queue_bind(
+                exchange= self.__exchange,
+                queue= queue_name,
+                routing_key= self.__topic)
+
+            self.__channel.basic_consume(
+                queue = queue_name, on_message = callback
+            )
+
+            self.__channel.start_consuming()
+        except AMQPError as amqpError :
+            pass
+
+    @property
+    def topic(self):
+        return self.__topic
+
+    @topic.setter
+    def topic(self, value):
+        self.__topic = value
+
+    @property
+    def exchange(self):
+        return self.__exchange
+
+    @exchange.setter
+    def exchange(self, value):
+        self.__exchange = value
--- backend/rabbitmq_microservice/producers.py
+++ backend/rabbitmq_microservice/producers.py
@@ -1,23 +1,78 @@
 import pika
+from pika.exceptions import  AMQPError
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class BaseProducer:
-    def __init__(self, queue_name):
+    def __init__(self, queue_name = ''):
         self.__exchange_type = ''
         self.__host = os.getenv('RABBITMQ_HOST')
-        self.__connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.__host))
-        self.__channel = self.__connection.channel()
         self.__queue_name = queue_name
-        self.__channel.queue_declare(queue=self.__queue_name)
+        self.__connection = None
+        self.__channel = None
 
     def produce(self, message):
-        self.__channel.basic_publish(
-            exchange=self.__exchange_type,
-            routing_key=self.__queue_name,
-            body=message)
+        try:
+            self.__channel.basic_publish(
+                exchange=self.__exchange_type,
+                routing_key=self.__queue_name,
+                body=message)
+        except AMQPError as amqpError:
+            pass
+
+    def open_connection(self):
+        try:
+            self.__connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.__host))
+            self.__channel = self.__connection.channel()
+        except AMQPError as amqpError:
+            pass
 
     def close_connection(self):
-        self.__connection.close()
+        try:
+            self.__connection.close()
+        except AMQPError as amqpError:
+            pass
+
+    @property
+    def queue_name(self):
+        return self.__queue_name
+
+    @queue_name.setter
+    def queue_name(self, value):
+        self.__queue_name = value
+
+class TopicProducer(BaseProducer):
+
+    def __init__(self, topic= None, exchange = None):
+        super().__init__()
+        self.__topic = topic
+        self.__exchange = exchange
+
+    def produce(self, message):
+        try:
+            self.__channel.exchange_declare(exchange=self.__exchange, exchange_type='topic')
+            self.__channel.basic_publish(
+                exchange= self.__exchange,
+                routing_key= self.__topic,
+                body=message
+            )
+        except AMQPError as amqpError:
+            pass
+
+    @property
+    def topic(self):
+        return self.__topic
+
+    @topic.setter
+    def topic(self, value):
+        self.__topic = value
+
+    @property
+    def exchange(self):
+        return self.__exchange
+
+    @exchange.setter
+    def exchange(self, value):
+        self.__exchange = value
