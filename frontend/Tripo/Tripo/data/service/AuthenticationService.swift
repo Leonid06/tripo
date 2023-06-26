ba28b2e7fe9134bef2479902b327bbfb102c717e
Leonid06 <x.worklene@gmail.com>
2023-06-26 14:55:08 +0300

    _

--- frontend/Tripo/Tripo/data/service/AuthenticationService.swift
+++ frontend/Tripo/Tripo/data/service/AuthenticationService.swift
@@ -1,8 +1,15 @@
 //
-//  AutehenticationService.swift
+//  AuthenticationService.swift
 //  Tripo
 //
 //  Created by Leonid on 23.06.2023.
 //
 
 import Foundation
+
+
+class AuthenticationService(){
+    func loginUser(email: String, password: String, completion : )
+}
+
+
--- frontend/Tripo/Tripo/presentation/view/RegistrationView.swift
+++ frontend/Tripo/Tripo/presentation/view/RegistrationView.swift
@@ -5,4 +5,41 @@
 //  Created by Leonid on 23.06.2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct RegistrationView: View {
+    @State private var email = ""
+    @State private var password = ""
+
+    var body: some View {
+        VStack(spacing: 20) {
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 200, height: 40)
+
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 200, height: 40)
+
+            Button(action: {
+                // Handle login here
+            }) {
+                Text("Register")
+                    .frame(width: 100)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 50)
+                    .padding(.vertical, 10)
+                    .background(Color.blue)
+                    .cornerRadius(5)
+            }
+            Spacer()
+        }
+        .padding(.top, 100)
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
