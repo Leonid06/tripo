f550d6577edc069f57ab6b8bd1057f009f2c67ca
Leonid06 <x.worklene@gmail.com>
2023-07-03 20:37:03 +0300

    added create plan manually route

--- backend/external_api_microservice/routers/plan/create.py
+++ backend/external_api_microservice/routers/plan/create.py
@@ -1,16 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 from postgresql_microservice.schemas.plan.create import PlanManualCreateIn, PlanManualCreateOut
 from postgresql_microservice.crud.plan.create import save_plan_created_manually
+from postgresql_microservice.dependencies import get_async_session
 plan_create_router = APIRouter(
     prefix  = '/plan/create',
-    tags = ['plan', 'create']
+    tags = ['plan/create']
 )
 
 
 @plan_create_router.post('/manual')
-async def create_plan_manually(payload : PlanManualCreateIn) -> PlanManualCreateOut:
-    save_plan_created_manually(payload=payload)
+async def create_plan_manually(payload : PlanManualCreateIn, db : AsyncSession = Depends(get_async_session)) -> PlanManualCreateOut:
+    await save_plan_created_manually(payload=payload, db = db)
 
-    return PlanManualCreateOut(message = 'Plan was successfully created')
+    return PlanManualCreateOut(message='Plan was successfully created')
 
 
