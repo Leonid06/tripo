b46f0c6a456d63021b70e116add9bd1642b65ba7
Leonid06 <x.worklene@gmail.com>
2023-07-10 17:35:42 +0300

    added landmark/get route module

--- backend/external_api_microservice/routers/landmark/save.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-from postgresql_microservice.schemas.plan.create import PlanManualCreateIn, PlanManualCreateOut
-from postgresql_microservice.crud.plan.create import save_plan_created_manually
-from postgresql_microservice.dependencies import get_async_session
-plan_create_router = APIRouter(
-    prefix  = '/plan/create',
-    tags = ['plan/create']
-)
