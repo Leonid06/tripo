550e07f57fac5df54960ab0a9b48b554c9e1a72e
Leonid06 <x.worklene@gmail.com>
2023-07-11 12:37:08 +0300

    _

--- backend/external_api_microservice/routers/landmark/get.py
+++ backend/external_api_microservice/routers/landmark/get.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from postgresql_microservice.dependencies import get_async_session
 from postgresql_microservice.schemas.landmark.get import GetLandmarkIn, GetLandmarkOut
-from rabbitmq_microservice.consumers import TopicConsumer
-from rabbitmq_microservice.producers import TopicProducer
+from landmark_api_microservice.cache.cacher.search_cacher import SearchCacher
+# from rabbitmq_microservice.consumers import TopicConsumer
+# from rabbitmq_microservice.producers import TopicProducer
+#
+# from external_api_microservice.config import RABBITMQ_MAIN_EXCHANGE_NAME, \
+#     RABBITMQ_LANDMARK_GET_REQUEST_TOPIC_NAME
 
-from external_api_microservice.config import RABBITMQ_MAIN_EXCHANGE_NAME, \
-    RABBITMQ_LANDMARK_GET_REQUEST_TOPIC_NAME
 
 
 landmark_get_router = APIRouter(
