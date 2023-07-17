2b95cf987f41e1c4b8d3d2c04e9c2132f1d17fd5
Leonid06 <x.worklene@gmail.com>
2023-07-17 18:42:51 +0300

    added landmark search request subscription functionality to search worker, refactored workers

--- backend/datasource/worker/search_worker.py
+++ backend/datasource/worker/search_worker.py
@@ -2,16 +2,14 @@ import asyncio
 import logging
 
 from broker.broker_client.base_broker_client import BaseBrokerClient
-from broker.exception import BrokerClientDataError, BrokerClientConnectionError
 
 from datasource.worker.base_worker import BaseWorker
 from datasource.networking_client.search_networking_client import SearchNetworkingClient
 from datasource.cache.cacher.search_cacher import SearchCacher
-from datasource.worker.search_worker_util import landmark_get_request_topic_consume_callback, \
-    map_landmark_get_request_message_body_to_identification_list, \
-    map_fuzzy_search_response_units_to_serialized_landmark_get_response_message_body, \
-    compose_error_landmark_get_response_body
-from datasource.exception import MappingError, DataError, CacheError, WorkerResponseError
+from datasource.worker.base_worker_util import basic_consume_callback
+from datasource.worker.search_worker_util import map_landmark_get_request_message_body_to_identification_list, \
+    map_fuzzy_search_response_units_to_response_message_body
+from datasource.exception import MappingError, DataError, CacheError, NetworkError, WorkerResponseError
 
 logger = logging.getLogger(__name__)
 
@@ -41,54 +39,28 @@ class SearchWorker(BaseWorker):
         try:
             identification_list = map_landmark_get_request_message_body_to_identification_list(
                 body=request_message_body)
-            units = self._cacher.get_fuzzy_search_response_units_by_identification(identifications=identification_list)
-            serialized_body = map_fuzzy_search_response_units_to_serialized_landmark_get_response_message_body(
-                units=units)
+            fuzzy_search_response_units = self._cacher.get_fuzzy_search_response_units_by_identification(
+                identifications=identification_list)
+            serialized_message_body = map_fuzzy_search_response_units_to_response_message_body(
+                units=fuzzy_search_response_units)
         except (MappingError, DataError, CacheError) as error:
             raise WorkerResponseError from error
 
-        return serialized_body
+        return serialized_message_body
 
-    async def __subscribe_to_landmark_get_request_topic(self,
-                                                        request_topic_name,
-                                                        request_exchange_name,
-                                                        consumption_queue
-                                                        ):
+    def __produce_response_message_to_landmark_search_by_radius_request(self, request_message_body):
         try:
-            await self._broker_client.consume_message(
-                topic_name=request_topic_name,
-                exchange_name=request_exchange_name,
-                callback=landmark_get_request_topic_consume_callback,
-                consumption_queue=consumption_queue
+            fuzzy_search_response_units = self._networking_client.make_fuzzy_search_request(
+                query='',
+                latitude=request_message_body['latitude'],
+                longitude=request_message_body['longitude']
             )
-        except (BrokerClientDataError, BrokerClientConnectionError) as error:
-            logger.exception(error)
-
-    async def __process_landmark_get_request(self,
-                                             response_exchange_name,
-                                             response_topic_name,
-                                             consumption_queue
-                                             ):
-
-        while True:
-            try:
-                request_message_body = await consumption_queue.get()
-
-                response_message_body = self.__produce_response_message_to_landmark_get_request(
-                    request_message_body=request_message_body)
-
-            except WorkerResponseError as error:
-                logger.exception(error)
-                response_message_body = compose_error_landmark_get_response_body()
-
-            try:
-                await self._broker_client.send_message(
-                    topic_name=response_topic_name,
-                    exchange_name=response_exchange_name,
-                    message_body=response_message_body
-                )
-            except (BrokerClientDataError, BrokerClientConnectionError) as error:
-                logger.exception(error)
+            serialized_message_body = map_fuzzy_search_response_units_to_response_message_body(
+                units=fuzzy_search_response_units
+            )
+        except (MappingError, NetworkError, DataError, KeyError) as error:
+            raise WorkerResponseError from error
+        return serialized_message_body
 
     async def subscribe_to_landmark_get_request(self,
                                                 request_exchange_name,
@@ -98,14 +70,39 @@ class SearchWorker(BaseWorker):
         consumption_queue = asyncio.Queue()
 
         await asyncio.gather(
-            self.__subscribe_to_landmark_get_request_topic(
+            self._subscribe_to_topic(
                 request_exchange_name=request_exchange_name,
                 request_topic_name=request_topic_name,
-                consumption_queue=consumption_queue
+                consumption_queue=consumption_queue,
+                callback=basic_consume_callback
             ),
-            self.__process_landmark_get_request(
+            self._process_request(
                 response_exchange_name=response_exchange_name,
                 response_topic_name=response_topic_name,
-                consumption_queue=consumption_queue
+                consumption_queue=consumption_queue,
+                response_produce_method=self.__produce_response_message_to_landmark_get_request
+            )
+        )
+
+    async def subscribe_to_landmark_search_by_radius_request(self,
+                                                             request_exchange_name,
+                                                             response_exchange_name,
+                                                             request_topic_name,
+                                                             response_topic_name,
+                                                             ):
+        consumption_queue = asyncio.Queue()
+
+        await asyncio.gather(
+            self._subscribe_to_topic(
+                request_exchange_name=request_exchange_name,
+                request_topic_name=request_topic_name,
+                consumption_queue=consumption_queue,
+                callback=basic_consume_callback
+            ),
+            self._process_request(
+                response_exchange_name=response_exchange_name,
+                response_topic_name=response_topic_name,
+                consumption_queue=consumption_queue,
+                response_produce_method=self.__produce_response_message_to_landmark_search_by_radius_request
             )
         )
--- backend/datasource/worker/search_worker_util.py
+++ backend/datasource/worker/search_worker_util.py
@@ -2,15 +2,7 @@ import json
 from datasource.exception import MappingError
 
 
-async def landmark_get_request_topic_consume_callback(message, callback_asyncio_queue):
-    try:
-        deserialized_message_body = json.loads(message.body)
-        await callback_asyncio_queue.put(deserialized_message_body)
-    except json.JSONDecodeError as error:
-        raise MappingError from error
-
-
-def map_fuzzy_search_response_units_to_serialized_landmark_get_response_message_body(units):
+def map_fuzzy_search_response_units_to_response_message_body(units):
     body = {'landmark': []}
 
     try:
@@ -40,6 +32,3 @@ def map_landmark_get_request_message_body_to_identification_list(body):
     return identifications
 
 
-def compose_error_landmark_get_response_body():
-    body = {'error': 'error'}
-    return json.dumps(body)
