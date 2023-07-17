f1790322967a96c5f343edae9c50f248c00ca945
Leonid06 <x.worklene@gmail.com>
2023-07-17 20:30:10 +0300

    modified topic names

--- backend/datasource/main.py
+++ backend/datasource/main.py
@@ -1,7 +1,8 @@
 import asyncio
 from datasource.worker.search_worker import SearchWorker
 from datasource.config import BASE_URL, API_KEY, \
-    RABBITMQ_LANDMARK_GET_BY_ID_RESPONSE_TOPIC_NAME, RABBITMQ_LANDMARK_GET_BY_ID_REQUEST_TOPIC_NAME, \
+    RABBITMQ_LANDMARK_GET_BY_ID_RESPONSE_TOPIC_NAME, RABBITMQ_LANDMARK_GET_BY_ID_REQUEST_TOPIC_NAME,\
+    RABBITMQ_SEARCH_LANDMARK_BY_RADIUS_REQUEST_TOPIC_NAME, RABBITMQ_SEARCH_LANDMARK_BY_RADIUS_RESPONSE_TOPIC_NAME,\
     RABBITMQ_MAIN_EXCHANGE_NAME, REDIS_HOST, REDIS_PORT, RABBITMQ_HOST, RABBITMQ_USER, RABBITMQ_PASSWORD
 
 
@@ -25,8 +26,8 @@ async def main():
         worker.subscribe_to_landmark_search_by_radius_request(
             request_exchange_name=RABBITMQ_MAIN_EXCHANGE_NAME,
             response_exchange_name=RABBITMQ_MAIN_EXCHANGE_NAME,
-            request_topic_name=RABBITMQ_LANDMARK_GET_BY_ID_REQUEST_TOPIC_NAME,
-            response_topic_name=RABBITMQ_LANDMARK_GET_BY_ID_RESPONSE_TOPIC_NAME
+            request_topic_name=RABBITMQ_SEARCH_LANDMARK_BY_RADIUS_REQUEST_TOPIC_NAME,
+            response_topic_name=RABBITMQ_SEARCH_LANDMARK_BY_RADIUS_RESPONSE_TOPIC_NAME
         )
     )
 
