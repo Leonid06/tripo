2e0985f5d601c488d15a4ffcf456301216ed82d1
Leonid06 <x.worklene@gmail.com>
2023-07-17 16:16:15 +0300

    modified requirements.txt in rest

--- backend/rest/test/landmark/landmark_get_test.py
+++ backend/rest/test/landmark/landmark_get_test.py
@@ -1,16 +1,31 @@
-from rest.main import app
 from fastapi.testclient import TestClient
+import redis
+from redis import Redis
+from uuid import uuid4
+
+from rest.main import app
+from rest.config import TEST_REDIS_HOST, TEST_REDIS_PORT
+
+
 
 client = TestClient(app)
 
+test_redis = Redis(host=TEST_REDIS_HOST, port=TEST_REDIS_PORT, decode_responses=True)
+
 
 def test_get_landmark_by_id_endpoint():
+    data = {
+        'name' : 'test_name'
+    }
+    test_id = str(uuid4())
+    test_redis.hset(test_id, mapping= data)
+
     response = client.post(
-        '/landmark/getby-id',
+        '/landmark/get/by-id',
         json={
             'landmark': [
                 {
-                    'id': ''
+                    'id': test_id
                 }
             ]
         }
