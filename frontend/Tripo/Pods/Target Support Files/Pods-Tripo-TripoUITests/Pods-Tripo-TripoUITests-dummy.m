85df6ef4200162d0bf77f97b1ab303a8c97c34ac
Leonid06 <x.worklene@gmail.com>
2023-07-24 21:57:32 +0300

    removed pods dir

--- frontend/Tripo/Pods/Target Support Files/Pods-Tripo-TripoUITests/Pods-Tripo-TripoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tripo_TripoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Tripo_TripoUITests
-@end
--- frontend/Tripo/Pods/Target Support Files/Pods-TripoTests/Pods-TripoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TripoTests : NSObject
-@end
-@implementation PodsDummy_Pods_TripoTests
-@end
