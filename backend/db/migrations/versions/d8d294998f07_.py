424c7a1377cf382cb47ccb6d8f50640482c06912
Leonid06 <x.worklene@gmail.com>
2023-07-25 13:01:07 +0300

    modified alembic.ini

---
+++ backend/db/migrations/versions/d8d294998f07_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d8d294998f07
+Revises: 7ec3227b848b
+Create Date: 2023-07-25 12:59:06.929359
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd8d294998f07'
+down_revision = '7ec3227b848b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
