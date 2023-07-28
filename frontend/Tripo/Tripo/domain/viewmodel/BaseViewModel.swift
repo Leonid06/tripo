7568b93a6ab10ac7901fdd4d2497bc8b0d2b9aa3
Leonid06 <x.worklene@gmail.com>
2023-07-28 14:35:58 +0300

    added base viewmodel

---
+++ frontend/Tripo/Tripo/domain/viewmodel/BaseViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewModel.swift
+//  Tripo
+//
+//  Created by Leonid on 28.07.2023.
+//
+
+import Foundation
+
+
+class BaseViewModel : ObservableObject {
+    internal func instantiateDatabaseService()
+}
