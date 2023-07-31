5671278e8db03b23072d87483be0eb0677ef0d1d
Leonid06 <x.worklene@gmail.com>
2023-07-31 20:52:19 +0300

    added overloads for many to many db client methods

--- frontend/Tripo/Tripo/data/persistence/db_client/PlanToLandmarkDatabaseClient.swift
+++ frontend/Tripo/Tripo/data/persistence/db_client/PlanToLandmarkDatabaseClient.swift
@@ -31,6 +31,28 @@ class PlanToLandmarkDatabaseClient : BaseDatabaseClient {
         }, async_callback_closure: callback)
     }
     
+    func createPlanToLandmarkRelationship(planId : UUID, landmarkId: UUID, callback : @escaping (AsynchronousDataTransaction.Result<Void>) -> ()){
+        makeAsyncTransaction(async_db_interaction_closure: {
+            transaction -> () in
+            do {
+                var plan = try transaction.fetchOne(
+                    From<Plan>().where(\.$identifier == planId)
+                )
+                var landmark = try transaction.fetchOne(
+                    From<Landmark>().where(\.$identifier == landmarkId)
+                )
+                if let plan = plan, let landmark = landmark {
+                    var planToLandmark = transaction.create(Into<PlanToLandmark>())
+                    plan.planToLandmark.insert(planToLandmark)
+                    landmark.planToLandmark.insert(planToLandmark)
+                }
+        
+            } catch {
+                throw CoreStoreError(error)
+            }
+        }, async_callback_closure: callback)
+    }
+    
     func deletePlanToLandmarkRelationship(planRemoteId : String, landmarkRemoteId : String,
                                           callback : @escaping (AsynchronousDataTransaction.Result<Void>) -> ()){
         makeAsyncTransaction(async_db_interaction_closure: {
@@ -41,6 +63,35 @@ class PlanToLandmarkDatabaseClient : BaseDatabaseClient {
                 )
                 var landmark = try transaction.fetchOne(
                     From<Landmark>().where(\.$remoteId == landmarkRemoteId)
+                )
+                if let plan = plan, let landmark = landmark {
+                    var planToLandmark = try transaction.fetchOne(
+                        From<PlanToLandmark>().where(\.$plan == plan && \.$landmark == landmark)
+                    )
+                    
+                    if let planToLandmark = planToLandmark {
+                        plan.planToLandmark.remove(planToLandmark)
+                        landmark.planToLandmark.remove(planToLandmark)
+                    }
+                }
+        
+            } catch {
+                throw CoreStoreError(error)
+            }
+        }, async_callback_closure: callback)
+        
+    }
+    
+    func deletePlanToLandmarkRelationship(planId : UUID, landmarkId : UUID,
+                                          callback : @escaping (AsynchronousDataTransaction.Result<Void>) -> ()){
+        makeAsyncTransaction(async_db_interaction_closure: {
+            transaction -> () in
+            do {
+                var plan = try transaction.fetchOne(
+                    From<Plan>().where(\.$identifier == planId)
+                )
+                var landmark = try transaction.fetchOne(
+                    From<Landmark>().where(\.$identifier == landmarkId)
                 )
                 if let plan = plan, let landmark = landmark {
                     var planToLandmark = try transaction.fetchOne(
--- frontend/Tripo/Tripo/data/persistence/db_client/PlanToUserDatabaseClient.swift
+++ frontend/Tripo/Tripo/data/persistence/db_client/PlanToUserDatabaseClient.swift
@@ -30,6 +30,29 @@ class PlanToUserDatabaseClient : BaseDatabaseClient {
         }, async_callback_closure: callback)
     }
     
+    func createPlanToUserRelationship(planId : UUID, userCurrentToken : String, callback : @escaping (AsynchronousDataTransaction.Result<Void>) -> ()){
+        makeAsyncTransaction(async_db_interaction_closure: {
+            transaction -> () in
+            do {
+                var plan = try transaction.fetchOne(
+                    From<Plan>().where(\.$identifier == planId)
+                )
+                var user = try transaction.fetchOne(
+                    From<User>().where(\.$currentToken == userCurrentToken)
+                )
+                if let plan = plan, let user = user {
+                    var planToUser = transaction.create(Into<PlanToUser>())
+                    plan.planToUser.insert(planToUser)
+                    user.planToUser.insert(planToUser)
+                }
+        
+            } catch {
+                throw CoreStoreError(error)
+            }
+        }, async_callback_closure: callback)
+    }
+
+    
     func deletePlanToUserRelationship(planRemoteId : String, userCurrentToken: String,
                                           callback : @escaping (AsynchronousDataTransaction.Result<Void>) -> ()){
         makeAsyncTransaction(async_db_interaction_closure: {
@@ -37,6 +60,33 @@ class PlanToUserDatabaseClient : BaseDatabaseClient {
             do {
                 var plan = try transaction.fetchOne(
                     From<Plan>().where(\.$remoteId == planRemoteId)
+                )
+                var user = try transaction.fetchOne(
+                    From<User>().where(\.$currentToken == userCurrentToken)
+                )
+                if let plan = plan, let user = user {
+                    var planToUser = try transaction.fetchOne(
+                        From<PlanToUser>().where(\.$plan == plan && \.$user == user)
+                    )
+                    if let planToUser = planToUser {
+                        plan.planToUser.remove(planToUser)
+                        user.planToUser.remove(planToUser)
+                    }
+                }
+            } catch {
+                throw CoreStoreError(error)
+            }
+        }, async_callback_closure: callback)
+        
+    }
+    
+    func deletePlanToUserRelationship(planId : UUID, userCurrentToken: String,
+                                          callback : @escaping (AsynchronousDataTransaction.Result<Void>) -> ()){
+        makeAsyncTransaction(async_db_interaction_closure: {
+            transaction -> () in
+            do {
+                var plan = try transaction.fetchOne(
+                    From<Plan>().where(\.$identifier == planId)
                 )
                 var user = try transaction.fetchOne(
                     From<User>().where(\.$currentToken == userCurrentToken)
