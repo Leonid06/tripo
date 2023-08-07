1c39fa8f1aaedebd7058936fb963580f85e3293d
Leonid06 <x.worklene@gmail.com>
2023-08-07 18:28:16 +0300

    implemented

--- frontend/Tripo/Tripo/domain/viewmodel/LandmarkViewModel.swift
+++ frontend/Tripo/Tripo/domain/viewmodel/LandmarkViewModel.swift
@@ -6,7 +6,60 @@
 //
 
 import Foundation
+import CoreStore
 
 
 class LandmarkViewModel : BaseViewModel {
+    @Published var landmark : Landmark?
+    @Published var instantiationState : ViewModelState.DatabaseClientInstantiationState = .instantiationInProgress
+    @Published var fetchLandmarkRequestState : ViewModelState.RequestState = .requestSucceeded
+    var databaseClient : LandmarkDatabaseClient?
+    
+    override init() {
+        do {
+            databaseClient = try LandmarkDatabaseClient(version: MigrationUtil.currentVersion)
+            instantiationState = .instantiationSucceded
+        }catch DatabaseClientError.differentStorageExistsAtUrl(let url) {
+            print("Different storage exists at URL : \(url)")
+            instantiationState = .instantiationFailed
+        }catch DatabaseClientError.internalError(let error) {
+            print("Internal database client error happened : \(error)")
+            instantiationState = .instantiationFailed
+        }catch DatabaseClientError.unknownError {
+            print("unknown database client error happened")
+            instantiationState = .instantiationFailed
+        }catch {
+            print("unknown error happened")
+            instantiationState = .instantiationFailed
+        }
+    }
+    func fetchLandmarkByRemoteId(remoteId : String){
+        guard let databaseClient = databaseClient else {
+            self.fetchLandmarkRequestState = .requestFailed
+            return 
+        }
+        databaseClient.getLandmarkObjectByRemoteId(remoteId: remoteId){
+            result -> () in
+            switch result {
+            case .success(let landmark):
+                if let landmark = landmark {
+                    self.landmark = landmark
+                    self.fetchLandmarkRequestState = .requestSucceeded
+                }
+                self.fetchLandmarkRequestState = .requestFailed
+            case .failure(let error):
+                self.fetchLandmarkRequestState = .requestFailed
+                switch error {
+                case CoreStoreError.unknown:
+                    print("unknown database client error happened")
+                case CoreStoreError.persistentStoreNotFound(let entity):
+                    print("Persistent store featuring entity : \(entity) was not found")
+                case CoreStoreError.internalError(let error):
+                    print("internal database client error happened : \(error)")
+                default:
+                    print("unknown error happened")
+                }
+            }
+        }
+    }
 }
--- frontend/Tripo/Tripo/domain/viewmodel/PlanViewModel.swift
+++ frontend/Tripo/Tripo/domain/viewmodel/PlanViewModel.swift
@@ -12,62 +12,61 @@ import CoreStore
 
 class PlanViewModel : BaseViewModel {
     @Published var plan : Plan?
-    @Published var databaseClientInstantiationState : ViewModelState.DatabaseClientInstantiationState = .instantiationInProgress
-    @Published var fetchPlanByIdRequestState :
+    @Published var instantiationState : ViewModelState.DatabaseClientInstantiationState = .instantiationInProgress
+    @Published var fetchPlanRequestState :
     ViewModelState.RequestState = .requestSucceeded
-    private let id : String
     private var databaseClient : PlanDatabaseClient?
     
     
     init(id : String) {
-        self.id = id
         do {
             databaseClient = try PlanDatabaseClient(version: MigrationUtil.currentVersion)
-            databaseClientInstantiationState = .instantiationSucceded
+            instantiationState = .instantiationSucceded
         }catch DatabaseClientError.differentStorageExistsAtUrl(let url) {
             print("Different storage exists at URL : \(url)")
-            databaseClientInstantiationState = .instantiationFailed
+            instantiationState = .instantiationFailed
         }catch DatabaseClientError.internalError(let error) {
             print("Internal database client error happened : \(error)")
-            databaseClientInstantiationState = .instantiationFailed
+            instantiationState = .instantiationFailed
         }catch DatabaseClientError.unknownError {
             print("unknown database client error happened")
-            databaseClientInstantiationState = .instantiationFailed
+            instantiationState = .instantiationFailed
         }catch {
             print("unknown error happened")
-            databaseClientInstantiationState = .instantiationFailed
+            instantiationState = .instantiationFailed
         }
     }
     
-    func fetchPlanByRemoteId(id: String){
-        fetchPlanByIdRequestState = .requestInProgress
+    func fetchPlanByRemoteId(remoteId: String){
+        fetchPlanRequestState = .requestInProgress
         
-        if let databaseClient = databaseClient {
-            databaseClient.getPlanObjectByRemoteId(remoteId: id){
-                result -> () in
-                switch result {
-                case .success(let plan):
-                    if let plan = plan {
-                        self.plan = plan
-                        self.fetchPlanByIdRequestState = .requestSucceeded
-                    }
-                    self.fetchPlanByIdRequestState = .requestFailed
-                case .failure(let error):
-                    self.fetchPlanByIdRequestState = .requestFailed
-                    switch error {
-                    case CoreStoreError.unknown:
-                        print("unknown database client error happened")
-                    case CoreStoreError.persistentStoreNotFound(let entity):
-                        print("Persistent store featuring entity : \(entity) was not found")
-                    case CoreStoreError.internalError(let error):
-                        print("internal database client error happened : \(error)")
-                    default:
-                        print("unknown error happened")
-                    }
+        guard let databaseClient = databaseClient else {
+            fetchPlanRequestState = .requestFailed
+            return 
+        }
+        
+        databaseClient.getPlanObjectByRemoteId(remoteId: remoteId){
+            result -> () in
+            switch result {
+            case .success(let plan):
+                if let plan = plan {
+                    self.plan = plan
+                    self.fetchPlanRequestState = .requestSucceeded
+                }
+                self.fetchPlanRequestState = .requestFailed
+            case .failure(let error):
+                self.fetchPlanRequestState = .requestFailed
+                switch error {
+                case CoreStoreError.unknown:
+                    print("unknown database client error happened")
+                case CoreStoreError.persistentStoreNotFound(let entity):
+                    print("Persistent store featuring entity : \(entity) was not found")
+                case CoreStoreError.internalError(let error):
+                    print("internal database client error happened : \(error)")
+                default:
+                    print("unknown error happened")
                 }
             }
-        } else {
-            fetchPlanByIdRequestState = .requestFailed
         }
     }
 }
